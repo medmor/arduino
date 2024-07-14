e3cf5222baf68b22f76b14d41396d56502a9ee19
medmor <elmadioune@enekreb.com>
2024-07-14 14:43:00 +0100

    working for now

--- motor.h
+++ motor.h
@@ -1,69 +1,100 @@
+#include <Ticker.h>
 #include "log.h"
+
+Ticker accelerator;
+
+// For now my motor driver is malfonctioning. only output A is working.
+// So I'm commenting out the other pins.
+// Support only forward and backward movements.
+// I will fix it later.
 
 int M1_ENA = D7;
 int M1_IN1 = D1;
 int M1_IN2 = D2;
-int M2_ENA = D5;
-int M2_IN1 = D3;
-int M2_IN2 = D4;
+// int M2_ENA = D5;
+// int M2_IN1 = D3;
+// int M2_IN2 = D4;
 
-void setupMotores() {
-  pinMode(M1_ENA, OUTPUT);
-  pinMode(M2_ENA, OUTPUT);
-  pinMode(M1_IN1, OUTPUT);
-  pinMode(M1_IN2, OUTPUT);
-  pinMode(M2_IN1, OUTPUT);
-  pinMode(M2_IN2, OUTPUT);
-  digitalWrite(M1_IN1, LOW);
-  digitalWrite(M1_IN2, LOW);
-  digitalWrite(M2_IN1, LOW);
-  digitalWrite(M2_IN2, LOW);
+int MIN_SPEED = 200;
+int MAX_SPEED = 255;
+int speed = MIN_SPEED;
 
-  analogWrite(M1_ENA, 255);
-  analogWrite(M2_ENA, 255);
+void setupMotores()
+{
+    pinMode(M1_ENA, OUTPUT);
+    //   pinMode(M2_ENA, OUTPUT);
+    pinMode(M1_IN1, OUTPUT);
+    pinMode(M1_IN2, OUTPUT);
+    //   pinMode(M2_IN1, OUTPUT);
+    //   pinMode(M2_IN2, OUTPUT);
+    digitalWrite(M1_IN1, LOW);
+    digitalWrite(M1_IN2, LOW);
+    //   digitalWrite(M2_IN1, LOW);
+    //   digitalWrite(M2_IN2, LOW);
+
+    analogWrite(M1_ENA, speed);
+    //   analogWrite(M2_ENA, 255);
 }
 
+void accelerate()
+{
+    log("Accelerate");
+    if (speed < MAX_SPEED)
+    {
+        speed = speed + 10;
+        analogWrite(M1_ENA, speed);
+        // analogWrite(M2_ENA, 255);
+    }
+}
 
-void motor_forward(){
+void motor_forward()
+{
     log("Forward");
 
-    digitalWrite(M2_IN1, LOW);
-    digitalWrite(M2_IN2, HIGH);
     digitalWrite(M1_IN1, LOW);
     digitalWrite(M1_IN2, HIGH);
+    accelerator.attach(100, accelerate);
+    // digitalWrite(M2_IN1, LOW);
+    // digitalWrite(M2_IN2, HIGH);
 }
 
-void motor_backward(){
+void motor_backward()
+{
     log("Backward");
 
     digitalWrite(M1_IN1, HIGH);
     digitalWrite(M1_IN2, LOW);
-    digitalWrite(M2_IN1, HIGH);
-    digitalWrite(M2_IN2, LOW);
+    accelerator.attach(100, accelerate);
+    // digitalWrite(M2_IN1, HIGH);
+    // digitalWrite(M2_IN2, LOW);
 }
 
-void motor_left(){
-    log("Left");
+// void motor_left(){
+//     log("Left");
 
-    digitalWrite(M1_IN1, HIGH);
-    digitalWrite(M1_IN2, LOW);
-    digitalWrite(M2_IN1, LOW);
-    digitalWrite(M2_IN2, HIGH);
-}
+//     digitalWrite(M1_IN1, HIGH);
+//     digitalWrite(M1_IN2, LOW);
+//     digitalWrite(M2_IN1, LOW);
+//     digitalWrite(M2_IN2, HIGH);
+// }
 
-void motor_right(){
-    log("Right");
+// void motor_right(){
+//     log("Right");
 
-    digitalWrite(M1_IN1, LOW);
-    digitalWrite(M1_IN2, HIGH);
-    digitalWrite(M2_IN1, HIGH);
-    digitalWrite(M2_IN2, LOW);
-}
+//     digitalWrite(M1_IN1, LOW);
+//     digitalWrite(M1_IN2, HIGH);
+//     digitalWrite(M2_IN1, HIGH);
+//     digitalWrite(M2_IN2, LOW);
+// }
 
-void motor_stop(){
+void motor_stop()
+{
     log("Stop");
     digitalWrite(M1_IN1, LOW);
     digitalWrite(M1_IN2, LOW);
-    digitalWrite(M2_IN1, LOW);
-    digitalWrite(M2_IN2, LOW);
+    accelerator.detach();
+    speed = MIN_SPEED;
+    analogWrite(M1_ENA, speed);
+    // digitalWrite(M2_IN1, LOW);
+    // digitalWrite(M2_IN2, LOW);
 }
--- server.h
+++ server.h
@@ -1,44 +1,41 @@
 #include <ESP8266WebServer.h>
 #include "motor.h"
 
-
 const char webpage[] PROGMEM = R"=====(
 <html>
 	<head>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>Car Controller</title>
 	</head>
 <body>
-	<main>
-		<h1>Car Controller</h1>
-        <button onmousedown="postDirection('forward')" onmouseup="postDirection('stop')">Forward</button>
-        <button onmousedown="postDirection('backward')" onmouseup="postDirection('stop')">Backward</button>
-        <button onmousedown="postDirection('left')" onmouseup="postDirection('stop')">Left</button>
-        <button onmousedown="postDirection('right')" onmouseup="postDirection('stop')">Right</button>
+	<main style="display:flex;gap: 10px;flex-direction:column;max-width: 300px;margin: auto;">
+		<h1 style="text-align:center">Car Controller</h1>
+    <button onmousedown="postDirection('forward')" onmouseup="postDirection('stop')" style="height: 300px;border-radius: 50%;font-size: 10rem;">⮝</button>
+    <button onmousedown="postDirection('backward')" onmouseup="postDirection('stop')" style="height: 300px; font-size: 10rem;border-radius: 50%;">⮟</button>
 	</main>
-    <script>
-        function postDirection(url) {
-            fetch(window.location.href + url, {method: 'POST'})
-            .then(function(response) {
-                console.log(response);
-            })
-            .catch(function(error) {
-                console.log(error);
-            });
-        }
-    </script>
+  <script>
+      function postDirection(url) {
+          fetch(window.location.href + url, {method: 'POST'})
+          .then(function(response) {
+              console.log(response);
+          })
+          .catch(function(error) {
+              console.log(error);
+          });
+      }
+  </script>
 </body>
 </html>
 )=====";
 
-
 ESP8266WebServer server(80);
-void handle_connect() {
+void handle_connect()
+{
   server.send(200, "text/html", webpage);
 }
 
-
-void serverLoop(){
+void serverLoop()
+{
   server.handleClient();
 }
 
@@ -54,17 +51,17 @@ void handle_backward()
   server.send(200, "text/plain", "Backward");
 }
 
-void handle_left()
-{
-  motor_left();
-  server.send(200, "text/plain", "Left");
-}
+// void handle_left()
+// {
+//   motor_left();
+//   server.send(200, "text/plain", "Left");
+// }
 
-void handle_right()
-{
-  motor_right();
-  server.send(200, "text/plain", "Right");
-}
+// void handle_right()
+// {
+//   motor_right();
+//   server.send(200, "text/plain", "Right");
+// }
 
 void handle_stop()
 {
@@ -72,21 +69,19 @@ void handle_stop()
   server.send(200, "text/plain", "Stop");
 }
 
-
 void handle_NotFound()
 {
   server.send(404, "text/plain", "Not found");
 }
 
-
 void serverSetup()
 {
-    server.on("/", HTTP_GET, handle_connect);
-    server.on("/forward", HTTP_POST, handle_forward);
-    server.on("/backward", HTTP_POST, handle_backward);
-    server.on("/left", HTTP_POST, handle_left);
-    server.on("/right", HTTP_POST, handle_right);
-    server.on("/stop", HTTP_POST, handle_stop);
-    server.onNotFound(handle_NotFound);
-    server.begin();
+  server.on("/", HTTP_GET, handle_connect);
+  server.on("/forward", HTTP_POST, handle_forward);
+  server.on("/backward", HTTP_POST, handle_backward);
+  // server.on("/left", HTTP_POST, handle_left);
+  // server.on("/right", HTTP_POST, handle_right);
+  server.on("/stop", HTTP_POST, handle_stop);
+  server.onNotFound(handle_NotFound);
+  server.begin();
 }
