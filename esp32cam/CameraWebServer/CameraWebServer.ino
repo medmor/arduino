7114a7130145bedf09fe3fadfad38ae9c4a69be1
medmor <elmadioune@enekreb.com>
2024-07-27 17:17:41 +0100

    com

--- esp32cam/CameraWebServer/CameraWebServer.ino
+++ esp32cam/CameraWebServer/CameraWebServer.ino
@@ -13,7 +13,7 @@
 // ===================
 // Select camera model
 // ===================
-#define CAMERA_MODEL_WROVER_KIT // Has PSRAM
+//#define CAMERA_MODEL_WROVER_KIT // Has PSRAM
 // #define CAMERA_MODEL_ESP_EYE  // Has PSRAM
 // #define CAMERA_MODEL_ESP32S3_EYE // Has PSRAM
 // #define CAMERA_MODEL_M5STACK_PSRAM // Has PSRAM
@@ -22,7 +22,7 @@
 // #define CAMERA_MODEL_M5STACK_ESP32CAM // No PSRAM
 // #define CAMERA_MODEL_M5STACK_UNITCAM // No PSRAM
 // #define CAMERA_MODEL_M5STACK_CAMS3_UNIT  // Has PSRAM
-// #define CAMERA_MODEL_AI_THINKER // Has PSRAM
+#define CAMERA_MODEL_AI_THINKER  // Has PSRAM
 // #define CAMERA_MODEL_TTGO_T_JOURNAL // No PSRAM
 // #define CAMERA_MODEL_XIAO_ESP32S3 // Has PSRAM
 //  ** Espressif Internal Boards **
@@ -42,8 +42,7 @@ const char *password = "paEuECPP";
 void startCameraServer();
 void setupLedFlash(int pin);
 
-void setup()
-{
+void setup() {
   Serial.begin(115200);
   Serial.setDebugOutput(true);
   Serial.println();
@@ -69,7 +68,7 @@ void setup()
   config.pin_reset = RESET_GPIO_NUM;
   config.xclk_freq_hz = 20000000;
   config.frame_size = FRAMESIZE_UXGA;
-  config.pixel_format = PIXFORMAT_JPEG; // for streaming
+  config.pixel_format = PIXFORMAT_JPEG;  // for streaming
   // config.pixel_format = PIXFORMAT_RGB565; // for face detection/recognition
   config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
   config.fb_location = CAMERA_FB_IN_PSRAM;
@@ -78,23 +77,17 @@ void setup()
 
   // if PSRAM IC present, init with UXGA resolution and higher JPEG quality
   //                      for larger pre-allocated frame buffer.
-  if (config.pixel_format == PIXFORMAT_JPEG)
-  {
-    if (psramFound())
-    {
+  if (config.pixel_format == PIXFORMAT_JPEG) {
+    if (psramFound()) {
       config.jpeg_quality = 10;
       config.fb_count = 2;
       config.grab_mode = CAMERA_GRAB_LATEST;
-    }
-    else
-    {
+    } else {
       // Limit the frame size when PSRAM is not available
       config.frame_size = FRAMESIZE_SVGA;
       config.fb_location = CAMERA_FB_IN_DRAM;
     }
-  }
-  else
-  {
+  } else {
     // Best option for face detection/recognition
     config.frame_size = FRAMESIZE_240X240;
 #if CONFIG_IDF_TARGET_ESP32S3
@@ -109,23 +102,20 @@ void setup()
 
   // camera init
   esp_err_t err = esp_camera_init(&config);
-  if (err != ESP_OK)
-  {
+  if (err != ESP_OK) {
     Serial.printf("Camera init failed with error 0x%x", err);
     return;
   }
 
   sensor_t *s = esp_camera_sensor_get();
   // initial sensors are flipped vertically and colors are a bit saturated
-  if (s->id.PID == OV3660_PID)
-  {
-    s->set_vflip(s, 1);       // flip it back
-    s->set_brightness(s, 1);  // up the brightness just a bit
-    s->set_saturation(s, -2); // lower the saturation
+  if (s->id.PID == OV3660_PID) {
+    s->set_vflip(s, 1);        // flip it back
+    s->set_brightness(s, 1);   // up the brightness just a bit
+    s->set_saturation(s, -2);  // lower the saturation
   }
   // drop down frame size for higher initial frame rate
-  if (config.pixel_format == PIXFORMAT_JPEG)
-  {
+  if (config.pixel_format == PIXFORMAT_JPEG) {
     s->set_framesize(s, FRAMESIZE_QVGA);
   }
 
@@ -146,8 +136,7 @@ void setup()
   WiFi.begin(ssid, password);
   WiFi.setSleep(false);
 
-  while (WiFi.status() != WL_CONNECTED)
-  {
+  while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
@@ -161,8 +150,7 @@ void setup()
   Serial.println("' to connect");
 }
 
-void loop()
-{
+void loop() {
   // Do nothing. Everything is done in another task by the web server
   delay(10000);
 }
