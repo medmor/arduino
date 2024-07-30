d22eb0bc7476143271129c5b5f4c5d8197a7d229
medmor <enakreb@hotmail.com>
2024-07-30 19:05:01 +0100

    stuck with littlefs

---
+++ websockets_test/src/main.cpp
@@ -0,0 +1,63 @@
+#include <WiFi.h>
+#include <ESPAsyncWebServer.h>
+#include <LittleFS.h>
+
+const char *ssid = "ZTE_2.4G_YqmMRN";
+const char *password = "paEuECPP";
+
+AsyncWebServer server(80);
+AsyncWebSocket ws("/ws");
+
+void onWebSocketEvent(AsyncWebSocket *server, AsyncWebSocketClient *client,
+                      AwsEventType type, void *arg, uint8_t *data, size_t len)
+{
+  if (type == WS_EVT_CONNECT)
+  {
+    Serial.printf("WebSocket client #%u connected from %s\n",
+                  client->id(), client->remoteIP().toString().c_str());
+    client->text("Hello from ESP32 WebSocket Server");
+  }
+  else if (type == WS_EVT_DISCONNECT)
+  {
+    Serial.printf("WebSocket client #%u disconnected\n", client->id());
+  }
+  else if (type == WS_EVT_DATA)
+  {
+    Serial.printf("WebSocket data received from client #%u\n", client->id());
+    client->text("Echo: " + String((char *)data));
+  }
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+
+  Serial.println("Connected to WiFi");
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+
+  if (!LittleFS.begin(true))
+  {
+    Serial.println("LittleFS Mount Failed");
+    return;
+  }
+  server.serveStatic("/", LittleFS, "/").setDefaultFile("index.html");
+
+  ws.onEvent(onWebSocketEvent);
+  server.addHandler(&ws);
+
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+void loop()
+{
+  ws.cleanupClients();
+}
