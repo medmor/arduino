f6de3536c33c22cb93244ae97761aa06e90bdd4b
medmor <elmadioune@gmail.com>
2026-07-12 13:29:13 +0100

    Enhance Car class with light control and update method for LED blinking

--- esp32c3_car_ble/car.h
+++ esp32c3_car_ble/car.h
@@ -4,13 +4,18 @@
 class Car
 {
 public:
-    Car(int in1M1, int in2M1, int in1M2, int in2M2);
+    Car(int in1M1, int in2M1, int in1M2, int in2M2, int lightPin);
     void forward();
     void backward();
     void left();
     void right();
     void stop();
+    void update(unsigned long now);
 
 private:
     Motor motor1, motor2;
+    int lightPin;
+    bool moving = false;
+    unsigned long lastToggle = 0;
+    bool ledState = false;
 };
